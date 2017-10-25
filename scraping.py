cc43217e0d17bca1135813a5723a313cb559c306
shantanub007 <shantanub30@gmail.com>
2017-10-25 23:33:42 +0530

    First commit

---
+++ scraping.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = raw_input('Enter URL: ')
+
+# Getting the webpage, creating a Response object.
+response = requests.get(url)
+
+# Extracting the source code of the page.
+data = response.text
+
+# Passing the source code to BeautifulSoup to create a BeautifulSoup object for it.
+soup = BeautifulSoup(data, 'lxml')
+
+# Extracting all the <a> tags into a list.
+tags = soup.find_all('a')
+
+# Extracting URLs from the attribute href in the <a> tags.
+for tag in tags:
+    print(tag.get('href'))
